710d441aca61a5ceca9aedbae4146c38fc49b212
mine01 <patteera.banl@gmail.com>
2020-02-17 15:24:45 +0700

    ALL Lab Javascript

--- Day7_2-17-2020/index.js
+++ Day7_2-17-2020/index.js
@@ -1,5 +1,150 @@
 // console.log("Tong");
-let name, human ;
-name = 'Tong';
-human = name;
-console.log(human);
+
+// Lab1
+
+// let name, human ;
+// name = 'Tong';
+// human = name;
+// console.log(human);
+
+// --------------------------
+// Lab2
+
+// let myWallet, myParents, myAddress, ageOfUniverse;
+
+// myWallet = '2';
+// myParents = 'Por','Mare';
+// myAddress = 'Bangkok';
+// ageOfUniverse = '13.8 billion years';
+
+// console.log(myWallet,myParents,myAddress,ageOfUniverse);
+
+// -----------------------------
+
+// let name = "Codecamp";
+
+// console.log(`hello ${1}`);
+// console.log(`hello ${"name"}`);
+// console.log(`hello ${name}`);
+
+// -----------------------------------
+
+// Ex 2
+
+// let name ="Tong",age ="29",address="Bangkok";
+// let history = `My name is ${name}. 
+// I'm ${age} year's old. 
+// My Home is ${address}.`;
+// console.log(history);
+
+// ----------------------------------------
+// Ex.4.6.2
+
+
+
+console.log("" + 1 + 0)
+// 1
+
+console.log("" - 1 + 0)
+// -1
+
+console.log(true + false)
+// 1
+
+console.log(6 / "3")
+//NaN 
+
+console.log("2" * "3")
+//NaN 
+
+console.log(4 + 5 + "px")
+// 9px
+
+console.log("$" + 4 + 5)
+// $9
+
+console.log("4" - 2)
+// 4-2
+
+console.log("4px" - 2)
+// 4px-2
+
+console.log(7 / 0)
+// infinity
+
+console.log("  -9  " + 5)
+// -95
+
+console.log("  -9  " - 5)
+// -9-5
+
+console.log(null + 1)
+// 1
+
+console.log(undefined + 1)
+// 1
+
+console.log(" \t \n" - 2)
+// NaN
+
+// --------------------------------------
+// 5.7.1
+
+
+console.log(5 > 4)
+// true
+console.log("apple" > "pineapple")
+// true
+console.log("2" > "12")
+// false
+console.log(undefined == null)
+// true
+console.log(undefined === null)
+// false
+console.log("bee" < "be")
+// true
+console.log("bee" > "Bee")
+// false
+console.log("Bee" < "be")
+// true
+
+// ---------------------------------
+// Ex 6.5.2
+
+// let x = prompt("What you name?");
+
+// if(x =="Tong"){
+//   console.log("เก่งมาก");
+// }else{
+//   console.log("คุณไม่รู้จักชื่อฉัน");
+// }
+
+// ---------------------------------------
+// Ex 6.5.3
+
+// let grade = prompt("Your Grade is?",0);
+
+// if(grade >= 80){
+//   console.log("A");
+// }else if(grade >= 70){
+//   console.log("B");
+// }else if(grade >= 60){
+//   console.log("C");
+// }else if(grade >= 50){
+//   console.log("D");
+// }else{
+//   console.log("F");
+// }
+
+// -----------------------------------------
+// Ex 6.5.4
+
+let age = prompt("How old are you");
+let price;
+price = (age < 18) ? 2000 :3500 ;
+
+console.log(price);
+
+
+
+
